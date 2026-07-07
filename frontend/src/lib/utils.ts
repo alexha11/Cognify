17edff2cf072b8c1980b0b63db94cb452e2e6065
alexha11 <thanhduonghd114@gmail.com>
2026-07-07 15:37:22 +0300

    chore: remove border in register and login page & optional chain for getInitials

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -22,5 +22,5 @@ export function formatFileSize(bytes: number): string {
 }
 
 export function getInitials(firstName: string, lastName: string): string {
-  return `${firstName.charAt(0)}${lastName.charAt(0)}`.toUpperCase();
+  return `${firstName?.charAt(0)}${lastName?.charAt(0)}`.toUpperCase();
 }
